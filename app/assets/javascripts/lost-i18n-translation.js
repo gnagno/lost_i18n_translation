f43329368298806029d14fa5d6b0e2299fece967
Gnagno <gnagno@gnagno.it>
2011-07-15 12:36:53 +0200

    added count for translations missing

--- app/assets/javascripts/lost-i18n-translation.js
+++ app/assets/javascripts/lost-i18n-translation.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
+  var $translation_missing_count = $('.translation_missing').length;
+
   var $newdiv = $('<div id="lost-i18n-translation-div"/>');
+
+  var $tr_missing = $('<span id="lost-i18n-translation-tr-missing-count" />');
+  $tr_missing.append( $translation_missing_count );
+  $newdiv.append("Missing translations: " , $tr_missing );
+
   $('body').append($newdiv);
 });
 
