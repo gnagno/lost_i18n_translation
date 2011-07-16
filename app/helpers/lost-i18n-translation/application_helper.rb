792221fc19aa7609758d1a37bed6fe816bb4a780
Gnagno <gnagno@gmail.com>
2011-07-16 13:24:49 +0200

    removed useless and wrong code

--- app/helpers/lost-i18n-translation/application_helper.rb
+++ app/helpers/lost-i18n-translation/application_helper.rb
@@ -1,8 +1,4 @@
 module LostI18nTranslation
   module ApplicationHelper
-    def pippo(*args)
-      'ciao ciao'
-    end
   end
-  ApplicationHelper.send :include, LostI18nTranslation::ApplicationHelper
 end
