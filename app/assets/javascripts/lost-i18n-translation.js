8ba79b0f2dacc5f8bb6add68ea3e4506f7092304
Gnagno <gnagno@gmail.com>
2011-07-16 14:50:13 +0200

    added present translations count

--- app/assets/javascripts/lost-i18n-translation.js
+++ app/assets/javascripts/lost-i18n-translation.js
@@ -1,11 +1,17 @@
 $(document).ready(function() {
   var $translation_missing_count = $('.translation_missing').length;
+  var $translation_present_count = $('.translation_present').length;
 
   var $newdiv = $('<div id="lost-i18n-translation-div"/>');
 
   var $tr_missing = $('<span id="lost-i18n-translation-tr-missing-count" />');
   $tr_missing.append( $translation_missing_count );
   $newdiv.append("Missing translations: " , $tr_missing );
+
+
+  var $tr_present = $('<span id="lost-i18n-translation-tr-present-count" />');
+  $tr_present.append( $translation_present_count );
+  $newdiv.append("Present translations: ", $tr_present  );
 
   $('body').append($newdiv);
 });
--- lib/lost-i18n-translation/i18n_ext.rb
+++ lib/lost-i18n-translation/i18n_ext.rb
@@ -1,7 +1,19 @@
 module ActionView::Helpers::TranslationHelper
   alias :t_old :t
 
+  def add_span str
+    #TODO: add the title part, this will be used to identify the string
+    "<span class=\"translation_present\">#{str}</span>".html_safe
+  end
+
   def t(*args)
-    "prova: #{t_old(args)}"
+    r = /<span class="translation_missing" title="translation missing:/
+    str = t_old(args)
+
+    if str =~ r
+      return str
+    else
+      add_span str
+    end
   end
 end
