95f60861831b55790e42e70bba371669ded19ee6
Gnagno <gnagno@gmail.com>
2011-07-23 18:34:39 +0200

    first working attempt to extend I18n module

--- lib/lost-i18n-translation/i18n_ext.rb
+++ lib/lost-i18n-translation/i18n_ext.rb
@@ -1,19 +1,31 @@
+module I18n
+  class << self
+    alias :old_translate :translate
+  end
+end
+
 module ActionView::Helpers::TranslationHelper
   alias :t_old :t
 
   def add_span str
     #TODO: add the title part, this will be used to identify the string
-    "<span class=\"translation_present\">#{str}</span>".html_safe
+    "<span class=\"translation_present\" title=\"translation present:\">#{str}</span>".html_safe
+  end
+
+  class String
+    def add_span
+      "<span class=\"translation_present\">#{str}</span>".html_safe
+    end
   end
 
   def t(*args)
     r = /<span class="translation_missing" title="translation missing:/
-    str = t_old(args)
+    translation = t_old(args)
 
-    if str =~ r
-      return str
+    if translation =~ r
+      return translation
     else
-      add_span str
+      add_span translation.first
     end
   end
 end
